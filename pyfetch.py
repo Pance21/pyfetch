9434435f61d393d68d63831882cd3f58145e7142
Pance21 <pance@rogers.com>
2023-03-23 17:41:15 -0400

    made it faster
    made it faster by replacing 'cpuinfo' with 'cat cpuinfo'...

--- pyfetch.py
+++ pyfetch.py
@@ -83,12 +83,24 @@ if check_file:
 	print (bcolors.OKGREEN + "\tComputer vendor: " + bcolors.ENDC + productvnd + " " + producthost)
 else:
 	de_result = "try another method for vendor."
-		
-import cpuinfo
-from cpuinfo import get_cpu_info
-info = get_cpu_info()
-cores = info['count']
-print(bcolors.OKGREEN + "\tCPU: " + bcolors.ENDC + info['brand_raw'] + " (" + str(cores).strip() + " cores)") 
+
+#import cpuinfo
+#from cpuinfo import get_cpu_info
+# this works but is too slow!
+#info = get_cpu_info()
+#cores = info['count']
+#print(bcolors.OKGREEN + "\tCPU: " + bcolors.ENDC + info['brand_raw'] + " (" + str(cores).strip() + " cores)") 
+
+# same info as above, but much faster
+de_result = subprocess.run(['cat /proc/cpuinfo'], stdout=subprocess.PIPE, shell=True)
+de_result = de_result.stdout.decode('utf-8')
+de_result = de_result.replace('\n', '')
+de_result = de_result.replace('\t', '')
+xstart=de_result.find("model name")+12
+xstop=de_result.find("stepping")
+xstart2=de_result.find("siblings")+10
+xstop2=de_result.find("core id")
+print( bcolors.OKGREEN + "\tCPU: " + bcolors.ENDC + de_result[xstart:xstop]+ " (" + de_result[xstart2:xstop2] + " cores)") 
 
 de_result = subprocess.run(['grep MemTotal /proc/meminfo'], stdout=subprocess.PIPE, shell=True)
 de_result = de_result.stdout.decode('utf-8')
@@ -108,20 +120,27 @@ print(bcolors.OKGREEN + "\tNetwork Host Name: " + bcolors.ENDC + socket.gethostn
 
 # IP info
 myip = socket.gethostbyaddr(socket.gethostname())[2]
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 internalIP = s.getsockname()[0]
 s.close()
 
-externalIP4  = os.popen('curl -s v4.ident.me').readline()
-externalIP6  = os.popen('curl -s v6.ident.me').readline()
+#externalIP4  = os.popen('curl -s v4.ident.me').readline()  #slower
+externalIP4  = os.popen('curl -s ifconfig.me').readline()   #faster
+#externalIP6  = os.popen('curl -s v6.ident.me').readline()	#slower
+externalIP6  = os.popen('curl -s icanhazip.com').readline()	#faster
 print(bcolors.OKGREEN + "\tInternal eth0 IP: " + bcolors.ENDC + internalIP)
 print(bcolors.OKGREEN + "\tInternal lo IP: " + bcolors.ENDC , listToString(myip))
 print(bcolors.OKGREEN + "\tExternal v4 IP: " + bcolors.ENDC + externalIP4)
 print(bcolors.OKGREEN + "\tExternal v6 IP: " + bcolors.ENDC + externalIP6)
 
 print (bcolors.OKBLUE + "\t---- apps -------" + bcolors.ENDC)
+de_result = subprocess.run(['bash', '--version'], stdout=subprocess.PIPE)
+de_result = de_result.stdout.decode('utf-8')
+de_result = de_result.replace('\n', '')
+xstop=de_result.find("Copyright")
+print( bcolors.OKGREEN + "\tShell: " + bcolors.ENDC, de_result[:xstop])
+
 de_result = subprocess.run(['python3', '--version'], stdout=subprocess.PIPE)
 de_result = de_result.stdout.decode('utf-8')
 de_result = de_result.replace('\n', '')
